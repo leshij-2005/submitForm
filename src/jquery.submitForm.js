e5d657965837a9a88c1e8f6ac9347b49ed0cc35d
Alexey Ershov <alershov3@gmail.com>
2018-09-25 15:18:47 +0500

    update deps; add validator for number;

--- src/jquery.submitForm.js
+++ src/jquery.submitForm.js
@@ -76,6 +76,17 @@
 
       return valid;
     },
+    number: function(item) {
+      var value = $(item).val();
+      var valid = true;
+
+      if (value && !(/^\d+$/).test(value)) {
+        $(item).addClass('invalid');
+        valid = false;
+      }
+
+      return valid;
+    },
   };
 
   function searchField(node) {
